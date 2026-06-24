6951e088b63771a55d37d429b703417cf7e862b1
Mayo-Racek <mayo@powerfuldata.io>
2026-06-24 11:34:58 +0200

    Update site title and description in config
    Update site identity and metadata

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Global website configuration.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Mayo Racek | Data Analyst & Engineer";
+
+export const SITE_DESCRIPTION =
+  "Mayo Racek is a Data Analyst & Engineer who turns 17 years of measurement expertise into reliable & scalable data solutions.";
+
+export const GENERATE_SLUG_FROM_TITLE = true;
+
+export const TRANSITION_API = true;
